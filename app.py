a2b4bbea9c52cf9620c2a6912ad70f024b00570e
Thomas Taron <phakamatg@gmail.com>
2022-09-10 11:42:21 +0700

    Secondly push

--- app.py
+++ app.py
@@ -3,8 +3,12 @@ from flask import Flask,render_template,url_for,request,redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "1111"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 app.config['SQLALCHEMY_BINDS'] = {"financial":'sqlite:///financial.db'}
 db = SQLAlchemy(app)
@@ -27,10 +31,29 @@ class financial(db.Model):
 
     def __repr__(self):
         return '<Task %r> % self.id'
+        
+# Create a form class
+class NamerForm(FlaskForm):
+    name = StringField("What's your name",validators=[DataRequired()])
+    submit = SubmitField("Submit")
 
 @app.route('/', methods= ['POST','GET'])
 def index():
     return render_template('index.html')
+
+@app.route('/user/<name>', methods= ['POST','GET'])
+def user(name):
+    return render_template('user.html',user_name=name)
+
+@app.route('/name', methods = ['GET','POST'])
+def name():
+    name = None
+    form = NamerForm()
+    # Validation Form
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('name.html',name = name, form = form)
 
 @app.errorhandler(404)
 def page_not_found(e):
